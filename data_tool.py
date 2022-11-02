04d33f7432f9c042271193dd3ad53700f9476ca3
Andrew Goeden <andrewgoeden@gmail.com>
2022-11-02 16:23:18 -0400

    added helper file and started creating execution structure

---
+++ data_tool.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pandas
+
+# Class with functions to help data modification
+class DataTool():
+
+    def __init__(self) -> None:
+        pass
+
+    def get_labels():
+        raise(NotImplementedError)
+
+    def get_features():
+        raise(NotImplementedError)
+
+    def get_data():
+        raise(NotImplementedError)
